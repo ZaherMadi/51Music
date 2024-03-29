2e6a567e55cb526f6b3ee259ee5558b34a171e51
Zaher Madi <zahermadi@yahoo.fr>
2024-03-29 13:58:55 +0100

    V0.0.1

--- MyMusicsStats/App.js
+++ MyMusicsStats/App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button ,Text, View, Linking  } from 'react-native';
 
 export default function App() {
+    // URL pour l'authentification Spotify
+    const authUrl = 'https://accounts.spotify.com/authorize?client_id=YOUR_CLIENT_ID&response_type=token&redirect_uri=YOUR_REDIRECT_URI&scope=user-read-private';
+  
+    // Fonction pour gérer le clic sur le bouton
+    const handlePress = () => {
+      // Ouvre l'URL d'authentification Spotify dans le navigateur par défaut
+      Linking.openURL(authUrl);
+      console.log('Bouton cliqué !');
+    }
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>SpotyStats</Text>
       <StatusBar style="auto" />
+      <Button
+        title="Mon Bouton"
+        onPress={handlePress}
+      />
     </View>
   );
 }

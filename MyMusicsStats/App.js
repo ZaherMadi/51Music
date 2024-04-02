f3abf7046ed8b090150a370c89b7d212b98f2777
Zaher Madi <zahermadi@yahoo.fr>
2024-04-02 14:14:56 +0200

    Last version , web added, dont worked at all

--- MyMusicsStats/App.js
+++ MyMusicsStats/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Button ,Text, View   } from 'react-native';
+import { StyleSheet, Button ,Text, View , Linking  } from 'react-native';
 import {WebView} from 'react-native-webview';
 
 const clientId = "841be26997054e1a9473f010e4948b34"
@@ -8,32 +8,57 @@ const authorizationEndpoint = "https://accounts.spotify.com/authorize";
 const tokenEndpoint = "https://accounts.spotify.com/api/token";
 const scope = 'user-read-private user-read-email';
 
-const redirectUrl = "http://localhost:3000";
-const redirectUrl2 = "http://zaher.fr";
+const redirectUrl = "http://localhost:8081";
+
 
 export default function App() {
   const [showWebView, setShowWebView] = useState(false);
     // URL pour l'authentification Spotify
-    const authUrl = 'https://accounts.spotify.com/authorize?client_id=YOUR_CLIENT_ID&response_type=token&redirect_uri=YOUR_REDIRECT_URI&scope=user-read-private';
-    const authUrl2 = 'https://zaher.fr';
-
+    const authUrl = `${authorizationEndpoint}?client_id=${clientId}&response_type=token&redirect_uri=${encodeURIComponent(redirectUrl)}&scope=${encodeURIComponent(scope)}`;
     // Fonction pour gérer le clic sur le bouton
 
     // Fonction pour gérer le clic sur le bouton "Se connecter à Spotify"
   const handleLoginClick = () => {
-    redirectToSpotifyAuthorize();
     console.log('Bouton handleLoginClick cliqué !');
+
+    redirectToSpotifyAuthorize();
 
   };
 
   // Fonction pour rediriger vers la page d'authentification Spotify
   const redirectToSpotifyAuthorize = () => {
-    const authUrl = `${authorizationEndpoint}?client_id=${clientId}&response_type=token&redirect_uri=${encodeURIComponent(redirectUrl)}&scope=${encodeURIComponent(scope)}`;
-    setShowWebView(true);    
     console.log('Bouton setShowWebView cliqué !');
+    setShowWebView(true);    
 
   };
+
+  const LinkingredirectToSpotifyAuthorize = () => {
+    console.log('Bouton Linking cliqué !');
+    const authUrl = `${authorizationEndpoint}?client_id=${clientId}&response_type=token&redirect_uri=${encodeURIComponent(redirectUrl)}&scope=${encodeURIComponent(scope)}`;
   
+
+  
+  const handleRedirect = (event) => {
+    console.log('handleRedirect  !');
+    const url = event.url;
+    console.log('URL:', url);
+    if (url && url.startsWith(redirectUrl)) {
+      console.log('same url:');
+      const accessToken = url.split('access_token=')[1].split('&')[0];
+      console.log('Access Token:', accessToken);
+      Linking.removeEventListener('url', handleRedirect);
+    }
+    console.log('done !');
+  }
+
+  Linking.addEventListener('url', handleRedirect);
+
+  Linking.openURL(authUrl);
+  console.log('fin de la fonction :');
+
+
+};
+
   return (
     <View style={styles.container}>
       <Text>SpotyStats</Text>
@@ -42,6 +67,7 @@ export default function App() {
         <WebView
           source={{ uri: authUrl }}
           style={{ flex: 1 }}
+          onLoad={() => console.log('Page loaded successfully')}
           onNavigationStateChange={(navState) => {
             // Vérifie si l'URL a changé pour détecter la redirection avec le token
             if (navState.url.startsWith(redirectUrl)) {
@@ -55,11 +81,17 @@ export default function App() {
         />
       ) : (
         <Button
-          title="Login"
+          title="LoginWebView"
           onPress={handleLoginClick}
           
         />
       )}
+        <Button
+        title="LoginLinking"
+        onPress={LinkingredirectToSpotifyAuthorize}
+        
+      />
+      
       
     </View>
   );
@@ -68,7 +100,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'limegreen',
     alignItems: 'center',
     justifyContent: 'center',
   },

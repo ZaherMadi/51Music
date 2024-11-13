d5bf1d9b291091b7d624da229cc4d3a87c2c9e85
Zaher Madi <zahermadi@yahoo.fr>
2024-11-13 22:55:04 +0100

    top-level await fix using vite config

--- 51Spotistats/vite.config.js
+++ 51Spotistats/vite.config.js
@@ -4,5 +4,9 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   server: {
     port: 6510 
+  },
+  esbuild: {
+    target: 'esnext'  // Permet de supporter le top-level await
   }
 })
+

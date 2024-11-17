93e48b02b744f0fcd9b0468110d1b1eed76037f2
Zaher Madi <zahermadi@yahoo.fr>
2024-11-17 18:39:35 +0100

    SpotiStas vercel test2.1.7

--- 51Spotistats/vite.config.js
+++ 51Spotistats/vite.config.js
@@ -3,10 +3,14 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   server: {
-    port: 6510 
+    port: 6510,
+    open: true 
   },
   esbuild: {
     target: 'esnext'  // Permet de supporter le top-level await
+  },
+  build: {
+    outDir: 'dist'  // Dossier de sortie
   }
 })
 
